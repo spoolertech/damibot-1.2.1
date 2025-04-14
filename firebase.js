88a387b5a70ef0ee032874790e1ec455138b3f05
*spoolertech* <juan.rivero@spoolertech.com.ar>
2025-04-14 19:12:29 -0300

    Eliminar carpeta de sesión antigua

--- firebase.js
+++ firebase.js
@@ -1,12 +1,14 @@
 const admin = require('firebase-admin');
-const path = process.env.FIREBASE_KEY_PATH;
 
-const serviceAccount = require(path);
+// Obtener el contenido del archivo de credenciales de Firebase desde la variable de entorno
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
 
+// Inicializar Firebase Admin con las credenciales
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DB_URL
 });
 
 const db = admin.database();
+
 module.exports = db;
