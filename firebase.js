c4762dc3c0b5acc243b45fc3a9639dd123658a28
spoolertech <spoolertech@gmail.com>
2025-04-14 20:53:30 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,12 @@
 const admin = require('firebase-admin');
+const path = process.env.FIREBASE_KEY_PATH;
 
-// Obtener el contenido del archivo de credenciales de Firebase desde la variable de entorno
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
+const serviceAccount = require(path);
 
-// Inicializar Firebase Admin con las credenciales
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DB_URL
 });
 
 const db = admin.database();
-
 module.exports = db;
